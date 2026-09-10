1f87ed9267828a099f18641e5a2c680c85175004
Alexander Frank <167268313+MsSuns3t@users.noreply.github.com>
2026-09-10 07:42:33 +0200

    Added unstyled card component

---
+++ src/components/Card.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Card.css";
+
+export default function Card({ image, title, description }) {
+    return(
+        <div className="card">
+            <img src={image} alt={description} className="card-image" />
+            <div className="card-content">
+                <h2 className="card-title">{title}</h2>
+                <p className="card-description">{description}</p>
+                <button className="card-button">Meer info</button>
+            </div>
+        </div>
+    );
+}
